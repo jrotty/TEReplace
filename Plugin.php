879b775c8f0df1a43c8f579284a0dbe246e4a1d4
jrotty <bssf@qq.com>
2022-04-27 14:35:17 +0800

    Update Plugin.php

--- Plugin.php
+++ Plugin.php
@@ -6,7 +6,7 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
  * @package TEReplace
  * @author 泽泽
  * @version 1.3.0
- * @link http://zezeshe.com
+ * @link http://blog.zezeshe.com
  */
 class TEReplace_Plugin implements Typecho_Plugin_Interface
 {
